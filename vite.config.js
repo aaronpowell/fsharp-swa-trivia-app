372a60d7c32166e859f07285cbc7c30523f37e49
Aaron Powell <me@aaron-powell.com>
2021-07-14 05:02:30 +0000

    trying input source maps via rollup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default defineConfig({
   root: "src",
+  plugins: [sourcemaps()],
   build: {
     outDir: "../public",
     emptyOutDir: true,
